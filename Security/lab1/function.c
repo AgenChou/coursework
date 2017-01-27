1f7d4dc8479fe158d3559a7d35dfe7ed46802f74
Agata Borkowska <borkowska.agata37@gmail.com>
2017-01-27 14:49:08 +0000

    Adding first security lab, WIP

---
+++ Security/lab1/function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// function to evaluate x^2/3-5x+18
+double fn(int x) {
+	double result = (double) ( x * x) / 3 - 5 * x + 18;
+	return result;
+}
+
+int main(int argc, int *argv[]) {
+	printf("Input  Output\n");
+
+	int i;
+	for (i = 0; i <= 9; i++) {
+		printf("%d       %.4f\n", i, fn(i));
+	}
+	
+	// for formatting purposes, when i has 2 digits, we use one fewer space
+	// so the columns are aligned (note: yes, it doesn't scale)
+	for (i = 10; i <= 12; i++) {
+		printf("%d      %.4f\n", i, fn(i));
+	}
+}
