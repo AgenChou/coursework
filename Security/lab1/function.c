f6ea8f551e80b62cf9bbad68ab12d86b41b2a0bc
Agata Borkowska <borkowska.agata37@gmail.com>
2017-01-29 12:38:30 +0000

    correcting formatting in function.c and functionsin.c in Security lab1

--- Security/lab1/function.c
+++ Security/lab1/function.c
@@ -2,6 +2,8 @@
 
 // Ex. 1a: function to evaluate x^2/3-5x+18
 double fn(int x) {
+	// The function contains simple operations (after expanding x^2 to x*x)
+	// so there's no need for the math library.
 	double result = (double) (x * x) / 3 - 5 * x + 18;
 	return result;
 }
@@ -10,13 +12,7 @@ int main(int argc, char *argv[]) {
 	printf("Input  Output\n");
 
 	int i;
-	for (i = 0; i <= 9; i++) {
-		printf("%d       %.4f\n", i, fn(i));
-	}
-	
-	// for formatting purposes, when i has 2 digits, we use one fewer space
-	// so the columns are aligned (note: yes, it doesn't scale)
-	for (i = 10; i <= 12; i++) {
-		printf("%d      %.4f\n", i, fn(i));
+	for (i = 0; i <= 12; i++) {
+		printf("%d\t%.4f\n", i, fn(i));
 	}
 }
