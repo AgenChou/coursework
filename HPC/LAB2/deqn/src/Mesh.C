64e5c76e05b349e80eacc7274c7c82490e451cd0
Agata Borkowska <borkowska.agata37@gmail.com>
2017-02-05 17:48:02 +0000

    Further work on HPC Lab

--- HPC/LAB2/deqn/src/Mesh.C
+++ HPC/LAB2/deqn/src/Mesh.C
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <omp.h>
 
 #define POLY2(i, j, imin, jmin, ni) (((i) - (imin)) + ((j)-(jmin)) * (ni))
 
@@ -62,13 +63,22 @@ void Mesh::allocate()
 
     double xmin = min_coords[0];
     double ymin = min_coords[1];
-
-    for (int i=0; i < nx+2; i++) {
-        cellx[i]=xmin+dx[0]*(i-1);
-    }
-
-    for (int i = 0; i < ny+2; i++) {
-        celly[i]=ymin+dx[1]*(i-1);
+	
+    // FIRST CHANGE
+#pragma omp parallel sections
+    {
+#pragma omp sectionn
+	{
+    		for (int i=0; i < nx+2; i++) {
+        		cellx[i]=xmin+dx[0]*(i-1);
+    		}
+	}
+#pragma omp section
+	{
+    		for (int i = 0; i < ny+2; i++) {
+        		celly[i]=ymin+dx[1]*(i-1);
+    		}
+	}
     }
 }
 
